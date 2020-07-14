125e746cd426896f01f2b760d1c4a5f7faa715c2
greghowe79 <64290303+greghowe79@users.noreply.github.com>
2020-07-15 00:33:42 +0200

    aggiornato la nostra applicazione App component, sign in component e sign-up component con l' utilizzo di hooks

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
@@ -15,21 +15,11 @@ import Header from './components/header/header.component';
 import {selectCurrentUser } from './redux/user/user.selectors';
 import { checkUserSession } from './redux/user/user.actions';
 
-class App extends React.Component {
-
-  unsubscribeFromAuth = null
-
-  componentDidMount() {
-    const { checkUserSession } = this.props;
+const App = ({ checkUserSession, currentUser }) => {
+  useEffect(() => {
     checkUserSession();
-  }
-
-  componentWillUnmount() {
-    this.unsubscribeFromAuth();
-  }
-
-
-  render() {
+ }, [checkUserSession]);
+  
     return (
       <div>
         <Header />
@@ -41,18 +31,14 @@ class App extends React.Component {
             exact 
             path='/signin' 
             render={() => 
-              this.props.currentUser ? (
-                <Redirect to='/' />
-              ) : (
-                <SignInAndSignUpPage />
-              )
+              currentUser ? <Redirect to='/' /> : <SignInAndSignUpPage />  
             }
           />
         </Switch>
       </div>
     ); 
   }
-}
+
 
 const mapStateToProps = createStructuredSelector({
   currentUser: selectCurrentUser
